8458d31ad6ae5fb26b461a143a80ea83f6f99cfe
ALİ KEMAL DURKAYA <adurkaya@bandirma.edu.tr>
2023-11-06 13:47:19 +0300

    ui calisma

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,11 +1,11 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
