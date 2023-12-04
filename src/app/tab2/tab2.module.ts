d72eb5beb051b11d8bf9599e9242faa5a5ebafb4
ALİ KEMAL DURKAYA <adurkaya@bandirma.edu.tr>
2023-12-04 13:45:01 +0300

    goon

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -17,4 +17,5 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
   ],
   declarations: [Tab2Page]
 })
+
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,21 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
-  
-  myEmail: string = ""
-  myPass: string = ""
 
-  constructor() {}
+  myEmail: string = "";
+  myPass: string = "";
+ bilgi:string="";
+ hata:boolean=false;
+ dogru:boolean=false;
 
-  giris(){
-    if(this.myEmail=="t@t.com" && this.myPass=="1234")
-    {
-      console.log(this.myEmail+" Başarılı giriş yaptınız");
-    }
-    else{
+  constructor() { }
+
+  testfonksiyonu(){
+    this.myEmail="selam"
+  }
+
+  giris() {
+    
+    if (this.myEmail == "t@t.com" && this.myPass == "1234") {
+      console.log(this.myEmail + " Başarılı giriş yaptınız");
+    this.bilgi=this.myEmail + " Başarılı giriş yaptınız"
+      this.dogru=true;
+      this.hata=false;
+  }
+    else {
       console.log("Giriş bilgilerinizi kontrol ediniz")
+      this.bilgi="Hatalı Giriş"
+      this.hata=true;
+      this.dogru=false;
     }
+
   }
 
 }
