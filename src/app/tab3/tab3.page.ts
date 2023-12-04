05d5af690242cd15b355125dad81292905323e44
ALİ KEMAL DURKAYA <adurkaya@bandirma.edu.tr>
2023-12-04 11:24:17 +0300

    some form

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,8 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
 
-  constructor() {}
+export class Tab3Page {
+  myEmail: string = ""
+  myPass: string = ""
+  constructor() {
+  }
+
+  mesaj() {
+
+    console.log("Girdiğin email : " + this.myEmail)
+    console.log("Şifren : " + this.myPass)
+    
+  }
+
 
 }
