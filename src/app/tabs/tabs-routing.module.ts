0eaf5d65236d81022b5f59d75f4ad397fc82f3ad
ALİ KEMAL DURKAYA <adurkaya@bandirma.edu.tr>
2023-12-18 11:38:50 +0300

    blala

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'product',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
