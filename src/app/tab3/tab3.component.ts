24e0de5e55ac86ee57a83895408f98bffb50c47f
ALİ KEMAL DURKAYA <adurkaya@bandirma.edu.tr>
2023-12-18 13:33:08 +0300

    dynamic accordion

--- src/app/tab3/tab3.component.ts
+++ src/app/tab3/tab3.component.ts
@@ -5,10 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tab3.component.html',
   styleUrls: ['./tab3.component.scss'],
 })
-export class Tab3Component  implements OnInit {
+export class Tab3Component implements OnInit {
 
+  products = [
+    {
+    "name": "Ürün 1",
+    "image": "https://ionicframework.com/docs/img/demos/card-media.png",
+    "fiyat": "150 TL",
+    "bilgi": "Here's a small text description for the card content. Nothing more, nothing less."
+  }
+    , {
+    "name": "Ürün 2",
+    "image": "https://ionicframework.com/docs/img/demos/card-media.png",
+    "fiyat": "50 TL",
+    "bilgi": "Here's a small text description for the card content. Nothing more, nothing less."
+  },
+  {
+    "name": "Ürün 3",
+    "image": "https://ionicframework.com/docs/img/demos/card-media.png",
+    "fiyat": "10 TL",
+    "bilgi": "Here's a small text description for the card content. Nothing more, nothing less."
+  },
+  {
+    "name": "Ürün 4",
+    "image": "https://ionicframework.com/docs/img/demos/card-media.png",
+    "fiyat": "120 TL",
+    "bilgi": "Here's a small text description for the card content. Nothing more, nothing less."
+  }
+  ]
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,10 +1,10 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Tab3Component } from './tab3.component';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
